709651ae42380f28d5a703e114acd1d3b367d74e
Zach Miller <zzm634@gmail.com>
2020-12-18 09:56:44 -0500

    Adds the first thing that actually worked.

---
+++ src/zmodo_stream_fixer/StreamFixer.java
@@ -0,0 +1,183 @@
+package zmodo_stream_fixer;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.Method;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.charset.Charset;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class StreamFixer {
+
+	private static final int BUFFER_SIZE = 4096;
+
+	private static final int SIZE_OFFSET = 24;
+
+	/**
+	 * returns false if eof is reached, otherwise, repeatedly reads from the input
+	 * stream enough to exactly fill the buffer
+	 *
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	private static final boolean blockingRead(InputStream in, byte[] buffer, int offset, int length)
+			throws IOException, InterruptedException {
+
+		int bytesRead = 0;
+		while (true) {
+			if (Thread.interrupted()) {
+				throw new InterruptedException();
+			}
+
+			int r = in.read(buffer, bytesRead, length - bytesRead);
+			if (r == -1)
+				return false;
+			bytesRead += r;
+			if (bytesRead == length)
+				return true;
+		}
+	}
+
+	// Reads and discards the given number of bytes
+	private static final boolean blockingDiscard(InputStream in, int bytes) throws IOException {
+		while (bytes-- > 0) {
+			int r = in.read();
+			if (r == -1)
+				return false;
+		}
+		return true;
+	}
+
+	private static final byte[] OODC_HEADER = { 0x30, 0x30, 0x64, 0x63 };
+	private static final byte[] o1dc_header = { 0x30, 0x31, 0x64, 0x63 };
+
+	private static final String KEY = "D52FF00F33CA48D98AB5C3D038C5A2EA";
+
+	private static final int ENCRYPTED_BYTES = 256;
+
+	private static final boolean PRINT_HEX = false;
+
+	public static void main(String[] args)
+			throws IOException, InterruptedException, NoSuchAlgorithmException, NoSuchPaddingException,
+			InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+
+		final String key;
+		if(args.length == 1) {
+			key = args[0];
+		} else {
+			key = KEY;
+		}
+
+		final InputStream in;
+
+		if (args.length == 1) {
+			String filename = args[0];
+			in = new FileInputStream(filename);
+		} else if (args.length > 1) {
+			return;
+		} else {
+			in = System.in;
+		}
+
+		try {
+			byte header[] = new byte[4];
+			byte size[] = new byte[4];
+			ByteBuffer sizebb = ByteBuffer.wrap(size);
+			sizebb.order(ByteOrder.LITTLE_ENDIAN);
+			byte buffer[] = new byte[BUFFER_SIZE];
+			Cipher cipher = Cipher.getInstance("AES/CBC/NOPADDING");
+
+			while (!Thread.interrupted()) {
+				// read the header
+				if (!blockingRead(in, header, 0, 4))
+					return;
+
+				boolean encrypted = Arrays.equals(header, OODC_HEADER);
+
+				// read the size
+				if (!blockingRead(in, size, 0, 4))
+					return;
+
+				sizebb.rewind();
+				int chunkSize = sizebb.getInt();
+
+				// skip the rest of the header and get to the data
+
+				// read extra data and throw it away
+				if (!blockingDiscard(in, SIZE_OFFSET))
+					return;
+
+				// copy the chunk data
+
+				if (encrypted) {
+					// first 256 bytes are possibly encoded with aes-cbc
+
+					// decrypt 256 bytes, then continue with the rest
+					byte iv[] = new byte[16];
+					Arrays.fill(iv, (byte) 0);
+
+					IvParameterSpec ivS = new IvParameterSpec(iv);
+					SecretKeySpec skeySpec = new SecretKeySpec(KEY.getBytes(), "AES");
+
+					cipher.init(Cipher.DECRYPT_MODE, skeySpec, ivS);
+
+					byte encryptedData[] = new byte[ENCRYPTED_BYTES];
+					blockingRead(in, encryptedData, 0, ENCRYPTED_BYTES);
+
+					byte decryptedData[] = cipher.doFinal(encryptedData);
+
+					if (PRINT_HEX) {
+						System.out.print(byteToHex(decryptedData));
+					} else {
+						System.out.write(decryptedData, 0, ENCRYPTED_BYTES);
+					}
+					chunkSize -= ENCRYPTED_BYTES;
+				}
+
+				while (chunkSize > 0) {
+					int toRead = Math.min(BUFFER_SIZE, chunkSize);
+
+					if (!blockingRead(in, buffer, 0, toRead))
+						return;
+					if (PRINT_HEX) {
+						System.out.print(byteToHex(buffer, 0, toRead));
+					} else {
+						System.out.write(buffer, 0, toRead);
+					}
+					chunkSize -= toRead;
+				}
+
+			}
+
+		} finally {
+			in.close();
+		}
+
+	}
+
+	private static String byteToHex(byte[] data) {
+		return byteToHex(data, 0, data.length);
+	}
+
+	private static String byteToHex(byte[] data, int offset, int length) {
+		StringBuilder sb = new StringBuilder(data.length * 2);
+		for (int i = offset; i < offset + length; ++i) {
+			sb.append(Integer.toHexString(data[i]));
+		}
+		return sb.toString();
+	}
+
+}
